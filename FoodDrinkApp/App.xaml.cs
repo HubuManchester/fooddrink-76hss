b29ea6f9ff503b0f5f4d029c9ecf162dba589d01
76hss <1539893078@qq.com>
2026-06-01 17:51:10 +0800

    6.1

---
+++ FoodDrinkApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace FoodDrinkApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = new Window(new AppShell());
+
+        
+        if (DeviceInfo.Current.Platform == DevicePlatform.WinUI ||
+            DeviceInfo.Current.Platform == DevicePlatform.MacCatalyst)
+        {
+            window.Width = 520;
+            window.Height = 880;
+        }
+
+        return window;
+    }
+}
